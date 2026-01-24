4da3be76571bd654942ac14d0b14b55d5bd0bb6b
abhimittal17 <mabhi6444@gmail.com>
2026-01-24 17:24:08 +0530

    refactor: update Prisma configuration and client setup

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,24 +1,27 @@
-import { PrismaClient } from '../generated/prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg'; // Import the Pool
+import { PrismaClient } from "../generated/client";
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+// 1. Create the Pool specifically for the adapter
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString
 });
 
+// 2. Pass the pool to the adapter
 const adapter = new PrismaPg(pool);
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-  log: ['query', 'info', 'warn', 'error'],
-})
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    // Optional: Log queries to see if connection works
+    log: ['query', 'info', 'warn', 'error'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma
+export * from "../generated/prisma/client";
